eaeb91ae03f94346d82e0fede51e7d9d9a746964
Victor Vilela Duarte <victorvduarte@gmail.com>
2019-10-26 20:43:37 -0300

    Props, estados e Debug

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,18 +1,63 @@
 import React, { Component } from "react";
 
+import TechItem from "./TechItem";
 class TechList extends Component {
   state = {
-    techs: ["Node.js", "ReactJS", "React Native"]
+    newTech: "",
+    techs: []
+  };
+
+  componentDidMount() {
+    const techs = localStorage.getItem("techs");
+
+    if (techs) {
+      this.setState({ techs: JSON.parse(techs) });
+    }
+  }
+
+  componentDidUpdate(_prevProps, prevState) {
+    if (prevState.techs !== this.state.techs) {
+      localStorage.setItem("techs", JSON.stringify(this.state.techs));
+    }
+  }
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ""
+    });
+  };
+
+  handleDelete = tech => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) });
   };
 
   render() {
-    console.log(this.state);
     return (
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
-      </ul>
+      <>
+        <form onSubmit={this.handleSubmit}>
+          <ul>
+            {this.state.techs.map(tech => (
+              <TechItem
+                key={tech}
+                tech={tech}
+                onDelete={() => this.handleDelete(tech)}
+              />
+            ))}
+          </ul>
+          <input
+            type="text"
+            onChange={this.handleInputChange}
+            value={this.state.newTech}
+          />
+          <button type="submit">Enviar</button>
+        </form>
+      </>
     );
   }
 }
